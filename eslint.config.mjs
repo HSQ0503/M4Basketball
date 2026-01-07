b3a504a42f7a30f3450085ae6e701316ea1843e6
HSQ0503 <hsq0503@gmail.com>
2026-01-07 15:47:19 -0500

    Disables eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,12 +9,28 @@ const eslintConfig = [
   ...compat.config({
     extends: ["next/core-web-vitals", "next/typescript"],
     rules: {
+      // Disable all TypeScript rules
       "@typescript-eslint/no-unused-expressions": "off",
       "@typescript-eslint/no-non-null-asserted-optional-chain": "off",
       "@typescript-eslint/ban-ts-comment": "off",
       "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-unused-vars": "off",
+      
+      // Disable React rules
+      "react-hooks/exhaustive-deps": "off",
+      "react-hooks/rules-of-hooks": "off",
+      "react/no-unescaped-entities": "off",
+      "react/display-name": "off",
+      
+      // Disable general rules
       "prefer-const": "off",
-      "react-hooks/exhaustive-deps": "error",
+      "no-unused-vars": "off",
+      "no-console": "off",
+      "no-debugger": "off",
+      
+      // Disable Next.js specific rules
+      "@next/next/no-img-element": "off",
+      "@next/next/no-html-link-for-pages": "off",
     },
   }),
 ];
