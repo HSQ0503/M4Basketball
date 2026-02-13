c047c5e8d4e3a5e6851c6fb16bc942a89898def6
HSQ0503 <hsq0503@gmail.com>
2026-02-13 13:43:52 -0500

    More changes

--- src/layouts/partials/PricingSection.tsx
+++ src/layouts/partials/PricingSection.tsx
@@ -152,7 +152,7 @@ const PricingSection = ({
               )}
             </div>
 
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 max-w-5xl mx-auto">
+            <div className="grid grid-cols-1 sm:grid-cols-2 gap-6 max-w-4xl mx-auto">
               {training_groups.groups.map(
                 (
                   group: {
